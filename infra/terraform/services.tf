6a14bc3ddcbe2c0bf7548d1e75cdce56a1e4d9c4
LunaCbum <2001210736@huit.edu.vn>
2026-08-28 11:04:51 +0000

    feat(infra): manage required GCP services

---
+++ infra/terraform/services.tf
@@ -0,0 +1,20 @@
+locals {
+  workload_services = toset([
+    "artifactregistry.googleapis.com",
+    "bigquery.googleapis.com",
+    "pubsub.googleapis.com",
+    "run.googleapis.com",
+    "secretmanager.googleapis.com",
+  ])
+}
+
+resource "google_project_service" "workload" {
+  for_each = local.workload_services
+
+  project = var.project_id
+  service = each.value
+
+  deletion_policy            = "ABANDON"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
