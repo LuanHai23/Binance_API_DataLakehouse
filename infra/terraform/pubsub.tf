0dea9ee3789659fc8fd05c1ba18946eee110f366
LunaCbum <2001210736@huit.edu.vn>
2026-09-02 06:01:58 +0000

    feat(infra): add temporary Pub/Sub debug subscription

--- infra/terraform/pubsub.tf
+++ infra/terraform/pubsub.tf
@@ -19,3 +19,24 @@ resource "google_pubsub_topic_iam_member" "binance_ingestor_publisher" {
   role    = "roles/pubsub.publisher"
   member  = google_service_account.binance_ingestor.member
 }
+
+resource "google_pubsub_subscription" "binance_aggtrade_debug" {
+  project = var.project_id
+  name    = "binance-aggtrade-debug-dev"
+  topic   = google_pubsub_topic.binance_aggtrade_events.id
+
+  ack_deadline_seconds       = 30
+  message_retention_duration = "3600s"
+  retain_acked_messages      = false
+
+  enable_exactly_once_delivery = false
+  enable_message_ordering      = false
+
+  expiration_policy {
+    ttl = "86400s"
+  }
+
+  depends_on = [
+    google_project_service.workload["pubsub.googleapis.com"],
+  ]
+}
